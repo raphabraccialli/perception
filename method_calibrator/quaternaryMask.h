fe016f0c19523daf3b3ad088b038ceff447f251d
Livia <liviagcf@gmail.com>
2018-09-08 17:03:46 -0300

    cmake project no qt funcionando

---
+++ method_calibrator/quaternaryMask.h
@@ -0,0 +1,30 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+#include <iterator>
+#include <string>
+#include <vector>
+
+using namespace std;
+using namespace cv;
+
+#ifndef QUATERNARYMASK_H
+#define QUATERNARYMASK_H
+
+
+class quaternaryMask
+{
+public:
+    quaternaryMask(){}
+
+    int blackLMax, whiteLMin, greenHMean, greenHVar, greenSMin;
+
+    Mat blackMask, whiteMask, greenMask;
+
+    void setMask(int blackLMax, int whiteLMin, int greenHMean, int greenHVar, int greenSMin);
+    void generateMask(cv::Mat image);
+
+};
+
+#endif // QUATERNARYMASK_H
