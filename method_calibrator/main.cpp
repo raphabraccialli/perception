2a88cc41c2ea59873d3b9f68ace31adb68975c0f
Livia <liviagcf@gmail.com>
2018-11-07 11:58:10 -0200

    ajustes

--- method_calibrator/main.cpp
+++ method_calibrator/main.cpp
@@ -3,8 +3,10 @@
 #include "../classes/evaluator.hpp"
 #include "../classes/dilate.hpp"
 #include "../classes/independentMasks.hpp"
+#include <ctime>
 
 //#define DEBUG 1 //usar junto com debug da evaluator.cpp
+//#define CLOCK 1 //para calcular tempo de execução
 
 #define WHITE_L_MIN 50
 #define WHITE_S_MAX 60
@@ -12,14 +14,14 @@
 #define GREEN_L_MAX 170
 #define GREEN_H_MEAN 55
 #define GREEN_H_VAR 15
-#define GREEN_S_MIN 20
+#define GREEN_S_MIN 50
 
 #define MIN_RADIUS 5
 #define MAX_RADIUS 35
 
 #define DILATION 1
 
-#define N_OF_CANDIDATES 10
+#define N_OF_CANDIDATES 20
 
 #define RESIZE_FACTOR_MASK 0.5
 #define RESIZE_FACTOR_HOUGH 0.5
@@ -33,6 +35,22 @@
 #define A_BOTTOM 0.198
 #define B_BOTTOM -22.2
 
+
+#ifdef CLOCK
+    std::clock_t start;
+#endif
+
+void time_it_start(){
+    #ifdef CLOCK
+        start = std::clock();
+    #endif
+}
+
+void time_it_stop(){
+    #ifdef CLOCK
+        std::cout << "Time: " << (std::clock() - start) << std::endl;
+    #endif
+}
 
 struct param_set{
     double hough_param1, hough_param2;
@@ -53,8 +71,7 @@ int main(int argc, char *argv[]){
         return -1;
     }
 
-
-    cv::Mat frame, frame_resized;
+    cv::Mat frame;
     std::vector<cv::Vec3f> circles;
 
     independentMasks Mask;
@@ -83,13 +100,21 @@ int main(int argc, char *argv[]){
     ////////////////////////////////////////////////////////////
     // só roda se parametro for passado na execução
     if(std::atoi(argv[3])){
-        for(double hough_param1 = 40; hough_param1 < 41; hough_param1=hough_param1+2){
-            for(double hough_param2 = 7; hough_param2 < 8; hough_param2=hough_param2+2){
+        for(double hough_param1 = 56; hough_param1 < 58; hough_param1=hough_param1+2){
+            for(double hough_param2 = 1; hough_param2 < 3; hough_param2=hough_param2+2){
                 cap.set(CV_CAP_PROP_POS_FRAMES, 0);
-                houghCirclesContrast hough(hough_param1, hough_param2, (int)MIN_RADIUS/2, (int)MIN_RADIUS, (int)MAX_RADIUS);
-                evaluator evaluator(argv[2], 0.04, 10);
+                houghCirclesContrast hough(hough_param1, hough_param2, (int)MIN_RADIUS/2*RESIZE_FACTOR_HOUGH, (int)MIN_RADIUS*RESIZE_FACTOR_HOUGH, (int)MAX_RADIUS*RESIZE_FACTOR_HOUGH);
+                
+                //Escolhe a câmera
+                #if TOP_CAMERA
+                    evaluator evaluator(argv[2], A_TOP, B_TOP);
+                #else
+                    evaluator evaluator(argv[2], A_BOTTOM, B_BOTTOM);
+                #endif
+
                 std::cout << "hough_param1: " << hough_param1 << "\though_param2: " << hough_param2 << std::endl;
                 while(1){
+                    time_it_start();       
                     // Capture frame-by-frame in the specified frame rate fps_new
                     cap >> frame;
 
@@ -109,7 +134,6 @@ int main(int argc, char *argv[]){
                     // frame * greenMask para eliminar circulos fora do campo
                     //(e conferir se ajuda no processamento)
                     circles = hough.run(frame_for_hough_masked);
-
                     cv::Point center(-1, -1);
                     int radius = -1;
 
@@ -152,6 +176,8 @@ int main(int argc, char *argv[]){
                         cap >> frame;
                     }
 
+                    time_it_stop();
+
                 }
                 float hough_total = evaluator.evaluate()*100;
                 std::cout << "\though_total: " << hough_total << "%%";
@@ -179,10 +205,10 @@ int main(int argc, char *argv[]){
     ////////////////////////////////////////////////////////////
     // só roda se parametro for passado na execução
     if(std::atoi(argv[4])){
-        for(float pixel_param1 = 0.02; pixel_param1 < 0.30; pixel_param1=pixel_param1+0.02){
-            for(float pixel_param2 = 0.02; pixel_param2 < 0.10; pixel_param2=pixel_param2+0.02){
+        for(float pixel_param1 = 0.28; pixel_param1 < 0.30; pixel_param1=pixel_param1+0.02){
+            for(float pixel_param2 = 0.08; pixel_param2 < 0.10; pixel_param2=pixel_param2+0.02){
                 cap.set(CV_CAP_PROP_POS_FRAMES, 0);
-                houghCirclesContrast hough(best.hough_param1, best.hough_param2, (double)MIN_RADIUS/2, (int)MIN_RADIUS, (int)MAX_RADIUS);
+                houghCirclesContrast hough(best.hough_param1, best.hough_param2, (double)MIN_RADIUS/2*RESIZE_FACTOR_HOUGH, (int)MIN_RADIUS*RESIZE_FACTOR_HOUGH, (int)MAX_RADIUS*RESIZE_FACTOR_HOUGH);
                 //inicia pixelCountCheck com porcentagem mínima de branco e preto na area da bola
                 //pixel_param1 = branco      pixel_param2 => preto
                 pixelCountCheck pixelChecker(pixel_param1, pixel_param2);
@@ -197,14 +223,13 @@ int main(int argc, char *argv[]){
 
                 std::cout << "pixel_param1: " << pixel_param1 << "\tpixel_param2: " << pixel_param2 << std::endl;
                 while(1){
+                    time_it_start();
                     // Capture frame-by-frame in the specified frame rate fps_new
                     cap >> frame;
 
                     // If the frame is empty, break immediately
                     if (frame.empty())
                       break;
-
-                    cv::Mat frame_resized;
 
                     cv::Mat frame_for_mask, frame_for_hough, green_roi, frame_for_hough_masked;
 
@@ -260,6 +285,8 @@ int main(int argc, char *argv[]){
                     for(int i=0; i < skip; i++){
                         cap >> frame;
                     }
+
+                    time_it_stop();
 
                 }
                 float pixel_total = evaluator.evaluate()*100;
